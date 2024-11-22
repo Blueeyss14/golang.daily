956ecb63c568ede602c64209537e6704d8b7583a
unknown <delkanoberutu8@gmail.com>
2024-11-22 21:05:12 +0700

    yg penting commit

--- fetch data/main.go
+++ fetch data/main.go
@@ -1,6 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type User struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+}
+
+func fetchData() []User {
+	url := "https://reqres.in/api/users?page=2"
+	var response, err = http.Get(url)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return nil
+	}
+	defer response.Body.Close() //biar gak resource leak
+
+	var result struct {
+		Data []User `json:"data"`
+	}
+
+	err = json.NewDecoder(response.Body).Decode(&result)
+	if err != nil {
+		fmt.Println("Error decoding JSON:", err)
+		return nil
+	}
+
+	return result.Data
+}
 
 func main() {
 	users := fetchData()
@@ -9,7 +41,7 @@ func main() {
 		return
 	}
 
-	// for i := 0; i < len(users); i++ {
-	// 	fmt.Printf("Name: %s %s\nEmail: %s\n", users[i].FirstName, users[i].LastName, users[i].Email)
-	// }
+	for i := 0; i < len(users); i++ {
+		fmt.Printf("Name: %s %s\nEmail: %s\n", users[i].FirstName, users[i].LastName, users[i].Email)
+	}
 }
